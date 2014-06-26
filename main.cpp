449aeb9f3797163fc72df1d5b5288918f044337b
uyras <150030@mail.ru>
2014-06-27 10:38:25 +1100

    *

--- main.cpp
+++ main.cpp
@@ -10,38 +10,42 @@ int main()
     config::Instance()->srand(time(NULL));
     config::Instance()->set2D();
 
-    for (int j=0;j<100;j++){
-        PartArray *sys;
 
-        int n=3;//количество частиц в линейке
-        double space = config::Instance()->partR*4.;//расстояние между центрами частиц
+    PartArray *sys, *example;
 
-        sys = new PartArray((double)n*space,(double)n*space,1);//размер образца 4 радиуса, или 2 диаметра
+    int n=3,m=5;//количество частиц в линейке
+    double space = config::Instance()->partR*4.;//расстояние между центрами частиц
 
-        //бросаем частицы в шахматном порядке на линию
-        sys->dropChain(space);
-        StateMachineFree oldState(sys->state);
-        sys->save("1.dat");
+    example = new PartArray((double)n*space,(double)m*space,1);//размер образца 4 радиуса, или 2 диаметра
 
-        //разносим их до определенного уровня в любую сторону. Максимум: (расстояние между частицами/2)-радиус
-        double d=space/2.-config::Instance()->partR;
-        //d*=0.05;
-        Vect dir; //направление, в которое двигать частицу.
-        for(int i=0;i<sys->count();i++){
-            double longitude = ((double)config::Instance()->rand()/(double)config::Instance()->rand_max) * 2. * M_PI;
-            dir.x = d * cos(longitude);
-            dir.y = d * sin(longitude);
-            dir.z = 0;
-            sys->parts[i]->pos += dir;
+    //бросаем частицы в шахматном порядке на линию
+    example->dropChain(space);
+    StateMachineFree oldState(example->state);
+
+    double dMax = space/2.-config::Instance()->partR;
+    for (double d=0; d<=dMax; d+=dMax/100.){
+        int anomCount=0;
+        for (int j=0;j<100;j++){
+            sys = example->copy();
+
+            //разносим их до определенного уровня в любую сторону. Максимум: (расстояние между частицами/2)-радиус
+            Vect dir; //направление, в которое двигать частицу.
+            for(int i=0;i<sys->count();i++){
+                double longitude = ((double)config::Instance()->rand()/(double)config::Instance()->rand_max) * 2. * M_PI;
+                dir.x = d * cos(longitude);
+                dir.y = d * sin(longitude);
+                dir.z = 0;
+                sys->parts[i]->pos += dir;
+            }
+
+            double EBeforeGS=sys->calcEnergy1();
+            sys->setToGroundState();
+            if (!(oldState==sys->state)){
+                anomCount++;
+            }
+            delete sys;
         }
-
-
-        sys->setToGroundState();
-        if (!(oldState==sys->state)){
-            cout<<"found anomaly!"<<endl;
-            sys->state->draw();
-            sys->save("2.dat");
-        }
+        cout<<"d = "<<d<<"; c = "<<anomCount<<endl;
     }
 
 
