d5cd802a1a4a79ad8c307e685d80e4ddf60eb6cc
uyras <150030@mail.ru>
2014-06-27 09:01:35 +1100

    *

--- main.cpp
+++ main.cpp
@@ -10,40 +10,45 @@ int main()
     config::Instance()->srand(time(NULL));
     config::Instance()->set2D();
 
-    for (int j=0;j<100;j++){
-        PartArray *sys;
 
-        int n=3;//количество частиц в линейке
-        double space = config::Instance()->partR*4.;//расстояние между центрами частиц
+    int n=3;//количество частиц в линейке
+    double space = config::Instance()->partR*4.;//расстояние между центрами частиц
+    int anom=0;
 
-        sys = new PartArray((double)n*space,(double)n*space,1);//размер образца 4 радиуса, или 2 диаметра
+    for (double d=0.4;d<=space/2.-config::Instance()->partR;d+=(space/2.-config::Instance()->partR)/100.) //разносим их до определенного уровня в любую сторону. Максимум: (расстояние между частицами/2)-радиус
+    {
+        cout<<"; anom="<<anom<<endl;
+        cout<<"d="<<d;
+        anom=0;
+        for (int j=0;j<10000;j++){
+            PartArray *sys;
 
-        //бросаем частицы в шахматном порядке на линию
-        sys->dropChain(space);
-        StateMachineFree oldState(sys->state);
-        sys->save("1.dat");
+            sys = new PartArray((double)n*space,(double)n*space,1);//размер образца 4 радиуса, или 2 диаметра
 
-        //разносим их до определенного уровня в любую сторону. Максимум: (расстояние между частицами/2)-радиус
-        double d=space/2.-config::Instance()->partR;
-        //d*=0.05;
-        Vect dir; //направление, в которое двигать частицу.
-        for(int i=0;i<sys->count();i++){
-            double longitude = ((double)config::Instance()->rand()/(double)config::Instance()->rand_max) * 2. * M_PI;
-            dir.x = d * cos(longitude);
-            dir.y = d * sin(longitude);
-            dir.z = 0;
-            sys->parts[i]->pos += dir;
-        }
+            //бросаем частицы в шахматном порядке на линию
+            sys->dropChain(space);
+            StateMachineFree oldState(sys->state);
+            sys->save("1.dat");
 
 
-        sys->setToGroundState();
-        if (!(oldState==sys->state)){
-            cout<<"found anomaly!"<<endl;
-            sys->state->draw();
-            sys->save("2.dat");
+
+            Vect dir; //направление, в которое двигать частицу.
+            for(int i=0;i<sys->count();i++){
+                double longitude = ((double)config::Instance()->rand()/(double)config::Instance()->rand_max) * 2. * M_PI;
+                dir.x = d * cos(longitude);
+                dir.y = d * sin(longitude);
+                dir.z = 0;
+                sys->parts[i]->pos += dir;
+            }
+
+
+            sys->setToGroundState();
+            if (!(oldState==sys->state)){
+                anom++;
+            }
         }
     }
-
+    cout<<"; anom="<<anom<<endl;
 
     cout << "finish!" << endl;
     return 0;
