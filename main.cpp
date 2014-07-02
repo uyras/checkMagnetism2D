87888aa4291f4e379ad06c484077a4dc9dfe9da9
uyras <150030@mail.ru>
2014-07-03 00:30:08 +1100

    Версия для рассчета на кластере MPI

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <queue>
 #include "config.h"
 #include "PartArray.h"
 #include "mpi.h"
@@ -46,77 +47,107 @@ int main(int argc, char** argv)
         MPI_Finalize();
         return 0;
     }
-    cout<<rank<<": start MPI process"<<endl;
 
-    config::Instance()->srand(time(NULL));
     config::Instance()->set2D();
-
     config::Instance()->srand(time(NULL)+rank*size);config::Instance()->rand();
-    int exitCode = -1;
 
-    if (rank!=0){
-        int messageCount = (double)config::Instance()->rand()/(double)config::Instance()->rand_max * 10.;
-        cout<<rank<<": it will sended "<<messageCount<<" messages"<<endl;
-        //передаем случайное количество случайных сообщений
-        for (int i=0;i<messageCount;i++){
-            int message = (double)config::Instance()->rand()/(double)config::Instance()->rand_max * 900 + 100;
-            MPI_Send(&message,1,MPI_INT,0,0,MPI_COMM_WORLD);
-            cout<<rank<<": send code "<<message<<endl;
-        }
-
-        //выходим
-        MPI_Send(&exitCode,1,MPI_INT,0,0,MPI_COMM_WORLD);
-        cout<<rank<<": send code "<<exitCode<<endl;
-
-    } else {
-        int recv = 0;
-        MPI_Status stat;
-        int finished=0;
-        do{
-            MPI_Recv(&recv,1,MPI_INT,MPI_ANY_SOURCE,0,MPI_COMM_WORLD,&stat);
-            cout<<rank<<": recieve code "<<recv<<" from "<<stat.MPI_SOURCE<<endl;
-            if (recv==exitCode)
-                finished++;
-        } while (finished<(size-1));
-        cout<<rank<<": "<<"program may finish"<<endl;
-    }
-
-    /*PartArray *sys, *example;
-
-    int n=3,m=5;//количество частиц в линейке
+    //директивы
+    int n=3, m=7, //количество частиц в линейке
+            experimentCount=1000;
+    double intervalCount = 100.;
     double space = config::Instance()->partR*4.;//расстояние между центрами частиц
+    double dMax = space/2.-config::Instance()->partR;
+    int exitCode = -1; //код выхода из системы
+    double transBuff[4] = {0,0,0,0}; //буффер передачи. 0 - d, 1 - m, 2 - n(количество поворотов), 3 - с (количество попыток);
 
+    PartArray *sys, *example;
     example = new PartArray((double)n*space,(double)m*space,1);//размер образца 4 радиуса, или 2 диаметра
 
-    //бросаем частицы в шахматном порядке на линию
+    //бросаем частицы в шахматном порядке на линию и запоминаем состояние
     example->dropChain(space);
     StateMachineFree oldState(example->state);
 
-    //в первом потоке распределяем по подпроцессам задачи. В задаче2 параметра: d и fi.
-    if (rank==0){
-        for (double d=0.; d<=dMax; d+=dMax/100.){
-            MPI_
-        }
-    }
+    if (rank!=0){
+        //все задачи берут свои рассчеты и выполняют
+        int iteration = 0; //номер итерации
+        double d=0;
+        for (int i=0; i<intervalCount; i++){
+            double m=0.;
+            for (int k=0;k<intervalCount;k++){
+                if (iteration==rank-1){
 
-    double dMax = space/2.-config::Instance()->partR;
-    for (double d=0.; d<=dMax; d+=dMax/100.){
-        int anomCount=0;
-        for (int j=0;j<100;j++){
-            sys = example->copy();
 
-            moveSystemMRandomly(sys,0.3);
+                    //сам эксперимент
+                    int anomCount=0;
+                    for (int j=0;j<experimentCount;j++){
+                        sys = example->copy();
 
-            sys->setToGroundState();
-            if (!(oldState==sys->state)){
-                anomCount++;
+                        moveSystemMRandomly(sys,m);
+                        moveSystemPosRandomly(sys,d);
+
+                        sys->setToGroundState();
+
+                        if (!(oldState==sys->state)){
+                            anomCount++;
+                        }
+
+                        delete sys;
+                    }
+
+                    //отправляем данные в 1 поток
+                    transBuff[0] = d;
+                    transBuff[1] = m;
+                    transBuff[2] = (double)anomCount;
+                    transBuff[3] = (double)experimentCount;
+                    MPI_Send(&transBuff,4,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+                    //cout<<rank<<": send code "<<anomCount<<endl;
+
+                }
+                if (iteration==size-2)
+                    iteration=0;
+                else
+                    iteration++;
+
+                m+=M_PI_2/intervalCount;
             }
-            delete sys;
+            d+=dMax/intervalCount;
         }
-        cout<<"d = "<<d<<"; c = "<<anomCount<<endl;
+
+
+        //отправляем код завершения процесса
+        transBuff[0] = exitCode;
+        transBuff[1] = transBuff[2] = transBuff[3] = 0;
+        MPI_Send(&transBuff,4,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+        //cout<<rank<<": send code "<<exitCode<<endl;
+
+    } else {
+        ofstream f("checkMagnetism2DRes_3x7.dat");
+        f<<"d\tfi\tanom\tcount"<<endl;
+
+        MPI_Status stat;
+        int finished=0, transactionCount=0;
+        do{
+            MPI_Recv(&transBuff,4,MPI_DOUBLE,MPI_ANY_SOURCE,0,MPI_COMM_WORLD,&stat);
+            transactionCount++;
+            cout << transactionCount/(intervalCount*intervalCount+size-1)*100<<"% complete"<<endl;
+            //cout<<rank<<": recieve code "<<transBuff[2]<<" from "<<stat.MPI_SOURCE<<endl;
+            if (transBuff[0]==exitCode)
+                finished++;
+            else {
+
+                //сохраняем данные
+                f<<
+                    transBuff[0]<<"\t"<<
+                      transBuff[1]<<"\t"<<
+                      transBuff[2]<<"\t"<<
+                      transBuff[3]<<"\t"<<endl;
+
+            }
+        } while (finished<(size-1));
+        cout<<rank<<": "<<"program may finish"<<endl;
+        f.close();
     }
 
-    cout << "finish!" << endl;*/
     MPI_Finalize();
     return 0;
 }
